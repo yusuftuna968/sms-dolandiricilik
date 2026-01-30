847b3cb91af59e0adcc532ad8e216ef62487b97e
yusuftuna968 <yusuftunayurtdas123@gmail.com>
2026-01-30 22:48:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
+import os
+
+COUNTER_FILE = "counter.txt"
+
+if not os.path.exists(COUNTER_FILE):
+    with open(COUNTER_FILE, "w") as f:
+        f.write("0")
+
+with open(COUNTER_FILE, "r") as f:
+    count = int(f.read())
+
+count += 1
+
+with open(COUNTER_FILE, "w") as f:
+    f.write(str(count))
 import streamlit as st
 import joblib
 
 st.set_page_config(page_title="SMS Kontrol", page_icon="📱")
-
+st.info(f"👥 Bu site şu ana kadar {count} kez ziyaret edildi")
 model = joblib.load("sms_model.pkl")
 vectorizer = joblib.load("vectorizer.pkl")
 
@@ -21,3 +36,4 @@ if st.button("Analiz Et"):
             st.error("🚨 DOLANDIRICI SMS")
         else:
             st.success("✅ GÜVENLİ SMS")
+
